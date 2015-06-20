bf38b7853b66f672a472751415ab2c7dd169593b
shoetten <simon@hoetten.org>
2015-06-20 13:50:06 +0200

    removes edit dropdown menu, replacing it with just one button. Also starting editing functionality in map.js

--- js/map.js
+++ js/map.js
@@ -46,4 +46,56 @@ $(function() {
      $contextMenu.hide();
   });
 
+
+
+
+  /* edit features */
+  var featureOverlay = new ol.FeatureOverlay({
+    style: new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(255, 255, 255, 0.2)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: '#ffcc33',
+        width: 2
+      }),
+      image: new ol.style.Circle({
+        radius: 7,
+        fill: new ol.style.Fill({
+          color: '#ffcc33'
+        })
+      })
+    })
+  });
+  featureOverlay.setMap(map);
+
+  var modify = new ol.interaction.Modify({
+    features: featureOverlay.getFeatures(),
+    // the SHIFT key must be pressed to delete vertices, so
+    // that new vertices can be drawn at the same position
+    // of existing vertices
+    deleteCondition: function(event) {
+      return ol.events.condition.shiftKeyOnly(event) &&
+          ol.events.condition.singleClick(event);
+    }
+  });
+  map.addInteraction(modify);
+
+  var draw; // global so we can remove it later
+  function addInteraction() {
+    draw = new ol.interaction.Draw({
+      features: featureOverlay.getFeatures(),
+      type: /** @type {ol.geom.GeometryType} */ (typeSelect.value)
+    });
+    map.addInteraction(draw);
+  }
+
+  // click listener
+  $("#edit").click(function() {
+    $(this).parent().toggleClass("active");
+
+
+    return false;
+  });
+
 });
