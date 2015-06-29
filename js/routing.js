2492d147c73e61df13b6edec55350bcc3c17350c
JoMol <JoMol@users.noreply.github.com>
2015-06-29 08:58:51 +0200

    Routing-Funktion

--- js/routing.js
+++ js/routing.js
@@ -3,3 +3,22 @@ $(document).ready(function(){
     $(".planer").toggle("slow");
     });
 });
+
+var startSetzen = function(){
+	contrvar = 1;
+	alert(contrvar);			
+}
+
+var startMarkerSetzen = function(){
+	alert("start: "+ lon + ", " + lat);
+	
+}
+
+var zielSetzen = function(){
+	contrvar = 2;
+	alert(contrvar);
+}
+
+var zielMarkerSetzen = function(){
+	alert("ziel: "+ lon + ", " + lat);
+}
