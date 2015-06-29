79fe87f8ace92f4db60f4c6d4acb97c9d8d1d79d
JoMol <JoMol@users.noreply.github.com>
2015-06-29 15:36:35 +0200

    Update routing.js

--- js/routing.js
+++ js/routing.js
@@ -4,21 +4,59 @@ $(document).ready(function(){
     });
 });
 
+
+var startMarker = 0;
+var zielMarker = 0;
+
 var startSetzen = function(){
-	contrvar = 1;
-	alert(contrvar);			
+	contrvar = 1;		
 }
 
 var startMarkerSetzen = function(){
-	alert("start: "+ lon + ", " + lat);
-	
+	console.log("start: "+ lonStart + ", " + latStart);
+	startMarker = 1;		
 }
 
 var zielSetzen = function(){
 	contrvar = 2;
-	alert(contrvar);
 }
 
 var zielMarkerSetzen = function(){
-	alert("ziel: "+ lon + ", " + lat);
+	console.log("ziel: "+ lonZiel + ", " + latZiel);
+	zielMarker = 1;
+}
+
+var routeV;
+
+//Route abfragen und darstellen
+var routeAbfragen = function(){
+	if(startMarker ==1 && zielMarker ==1){
+		//starte abfrage:
+	var urlRoute = "http://openls.geog.uni-heidelberg.de/testing2015/route?Start=" + lonStart + "," + latStart + "&End=" + lonZiel + "," + latZiel + "&Via=&lang=de&distunit=KM&routepref=Fastest&avoidAreas=&useTMC=false&noMotorways=false&noTollways=false&instructions=false";
+	alert("Die Route wird berechnet.");
+	console.log("Abfage: " + urlRoute);
+			
+			routeV = new ol.layer.Vector({
+			  source: new ol.source.Vector({
+				format: new ol.format.GML(),
+				url: urlRoute
+			  }),
+			  style: new ol.style.Style({
+				symbolizers: [
+				  new ol.style.Fill({
+					color: '#ffffff',
+					opacity: 0.25,
+					strokeWidth: 4 
+				  }),
+				  new ol.style.Stroke({
+					color: '#6666ff'
+				  })
+				]
+			  })
+			});
+		console.log(routeV);
+	}
+	else {
+		alert("Sie müssen vorher den Start- und Endpunkt ihrer Route festlegen.");
+	}	
 }
